1f2a1e2780555377b1ccf406586d8849ff08b0d9
Dylan Curran <c00217112@itcarlow.ie>
2019-07-30 21:01:48 +0100

    First commit

---
+++ Autumn 2019 Tank Game Dylan Curran/Assets/_scripts/PosTextUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PosTextUI : MonoBehaviour
+{
+    public Text _posText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _posText.text = "Pos: 1/2";
+    }
+}
