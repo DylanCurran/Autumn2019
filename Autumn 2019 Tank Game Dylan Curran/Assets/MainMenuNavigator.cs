85cdb1f3c10afe8f9feac212317e826692015243
DylanCurran <dylcurran@gmail.com>
2019-08-14 16:55:50 +0100

    Basic Menu Implementation

---
+++ Autumn 2019 Tank Game Dylan Curran/Assets/MainMenuNavigator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuNavigator : MonoBehaviour
+{
+	public void PlayButton()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void ExitButton()
+	{
+		Debug.Log("Quit");
+	}
+}
